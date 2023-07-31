0397ebe08cb13f9b6b72ab8f1a6bc92bc973a742
eugenyb <eberkunsky@gmail.com>
2023-07-31 19:34:25 +0300

    Initial commit

---
+++ src/main/java/db/main/Main.java
@@ -0,0 +1,26 @@
+package db.main;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run() {
+        try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/demo3107", "student", "123")) {
+            try (PreparedStatement ps = connection.prepareStatement("select * from student");
+                 ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    int age = resultSet.getInt("age");
+                    System.out.println("id: " + id + ", name: " + name + ", age: " + age);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
